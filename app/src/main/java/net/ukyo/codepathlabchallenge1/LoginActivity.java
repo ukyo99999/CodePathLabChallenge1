c3c2d57bdcb3d05134265d9dd8f7c88ecbe4cd47
ukyo <ukyo99999@gmail.com>
2017-01-08 12:36:32 +0800

    When pressing action buttons, hook up the button event to show toast message.

--- app/src/main/java/net/ukyo/codepathlabchallenge1/LoginActivity.java
+++ app/src/main/java/net/ukyo/codepathlabchallenge1/LoginActivity.java
@@ -2,12 +2,59 @@ package net.ukyo.codepathlabchallenge1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity {
+    private EditText editEmail;
+    private EditText editPassword;
+    private TextView textForgetPassword;
+    private Button btnSignIn;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        findViews();
+        setListener();
     }
+
+    private void findViews() {
+        setContentView(R.layout.activity_login);
+        editEmail = (EditText) findViewById(R.id.edit_email);
+        editPassword = (EditText) findViewById(R.id.edit_password);
+        textForgetPassword = (TextView) findViewById(R.id.text_forget_password);
+        btnSignIn = (Button) findViewById(R.id.btn_sign_in);
+        btnRegister = (Button) findViewById(R.id.btn_register);
+    }
+
+    private void setListener() {
+        editEmail.setOnClickListener(clickListener);
+        editPassword.setOnClickListener(clickListener);
+        textForgetPassword.setOnClickListener(clickListener);
+        btnSignIn.setOnClickListener(clickListener);
+        btnRegister.setOnClickListener(clickListener);
+    }
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btn_sign_in:
+                    Toast.makeText(LoginActivity.this, "Sign in", Toast.LENGTH_SHORT).show();
+                    break;
+
+                case R.id.btn_register:
+                    Toast.makeText(LoginActivity.this, "Register", Toast.LENGTH_LONG).show();
+                    break;
+
+                case R.id.text_forget_password:
+                    Toast.makeText(LoginActivity.this, "Forget Password", Toast.LENGTH_LONG).show();
+                    break;
+            }
+        }
+    };
 }
